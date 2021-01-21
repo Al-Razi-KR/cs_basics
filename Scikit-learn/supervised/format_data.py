8e03b4299a14f6b549b5d1e685fe7515c016e017
Al-Razi-KR <mohammedrazikr9419@gmail.com>
2021-01-21 19:48:26 +0800

    adds notebook

--- Scikit-learn/supervised/format_data.py
+++ Scikit-learn/supervised/format_data.py
@@ -1,4 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.datasets import load_iris
 
-import sklearn
+
+# Load dataset
+data = load_iris()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['species'] = data.target
+
+# Arrange Data into Features Matrix and Target  Vector
+feature_names = [
+    'sepal length (cm)',
+    'sepal width (cm)',
+    'petal length (cm)',
+    'petal width (cm)',
+]
