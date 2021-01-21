af9067fbba691e6446a322391133ca341c6b1ff0
Al-Razi-KR <mohammedrazikr9419@gmail.com>
2021-01-21 18:47:08 +0800

    adds format_data.py

--- Lesson1/Titanic/train.py
+++ Lesson1/Titanic/train.py
@@ -4,7 +4,7 @@ import pandas as pd
 def main():
     train_csv_file = r"C:\Users\razik\cs50\ai_basics\Lesson1\Titanic\train.csv"
     train_data = pd.read_csv(train_csv_file)
-    print(train_data.head())
+    print(train_data)
 
 if __name__ == "__main__":
     main()
