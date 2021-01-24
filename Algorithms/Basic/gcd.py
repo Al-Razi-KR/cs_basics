fd298360c162c8d883fbe5cc7f5f94f5fc682654
Al-Razi-KR <mohammedrazikr9419@gmail.com>
2021-01-24 12:34:41 +0800

    edits gcd.py

--- Algorithms/Basic/gcd.py
+++ Algorithms/Basic/gcd.py
@@ -1,12 +1,25 @@
 # Find the greatest common denominater of two numbers using Euclid's algorithm
 
+def gcd1(a, b):
+    lowest = min(a,b)
+    while lowest != 0:
+        if a%lowest == 0 and b%lowest == 0:
+            return lowest
+        else:
+            lowest -= 1
+
 def gcd(a, b):
-    pass
+    while b != 0:
+        t, a = a, b
+        b = t % b
+    return a
+    
 
 def test_samplets():
-    print(f"Answer: 12; Result: {gcd(60, 92)}") # should return 12
-    print(f"Answer: 12; Result: {gcd(21, 42)}") # should return 7
-    print(f"Answer: 12; Result: {gcd(12, 20)}") # should return 4
+    print(f"Answer: 12; Result: {gcd(60, 96)}") # should return 12
+    print(f"Answer: 21; Result: {gcd(21, 42)}") # should return 21
+    print(f"Answer: 4; Result: {gcd(12, 20)}") # should return 4
+    print(f"Answer: 4; Result: {gcd(20, 8)}") # should return 4
 
 def main():
     test_samplets()
