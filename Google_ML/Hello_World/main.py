400550b844db288dee175267f446d01fa59767d3
Al-Razi <mohammedrazikr9419@gmail.com>
2021-02-03 14:32:56 +0800

    Update

--- Google_ML/Hello_World/main.py
+++ Google_ML/Hello_World/main.py
@@ -1,6 +1,13 @@
-import sklearn
+# This simple example uses decision tree algorithm to predict based on a simple data
+
+from sklearn import tree
 
 features = [[140, 1], [130, 1], [150, 0], [170, 0]] # 1 is smooth and 0 is bumpy
-labels = [0, 0, 1, 1] # 1 is apple and 0 is oranges
+labels = [0, 0, 1, 1] # 0 is apple and 1 is oranges
 
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(features, labels)
+
+print (clf.predict([[150, 1 ]]))
 
